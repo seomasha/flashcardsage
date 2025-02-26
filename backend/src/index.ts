e15ac8a7b636b4bd2c5bac7bd85675662796bb02
seadmasetic <75685855+seomasha@users.noreply.github.com>
2025-02-26 19:04:36 +0100

    Adding Deck Model and initial code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000.");
+app.use(express.json());
+
+const PORT = 3000;
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
+
+const db = mongoose
+  .connect(
+    "mongodb+srv://maseticsead:VTV27VGbofok90fx@cluster0.lijiq.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}.`);
+    });
+  });
