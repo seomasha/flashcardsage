b8d070169d148bffd4b4646e55918326fa7e46f5
seadmasetic <75685855+seomasha@users.noreply.github.com>
2025-02-26 18:40:27 +0100

    adding necessary dependencies and setting the project up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000.");
+app.listen(3000, () => {
+  console.log("Listening on port 3000.");
 });
